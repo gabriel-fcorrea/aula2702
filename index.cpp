52d15bc182824a6c51c8dd995ac86b28da7e710c
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-02-27 20:33:33 -0300

    soma basica

--- index.cpp
+++ index.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main ()
 {
-	cout << "Hello";
+
+	int a;
+	int b; 
+	int c;
+
+	cout << "Digite um numero abaixo: \n";
+	cin >> a;
+	cout << "Digite outro numero abaixo: \n";
+	cin >> b;
+	c = a + b;
+	cout << "O resultado e... " << c;
 }
