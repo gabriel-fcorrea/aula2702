0dc401fded542638689b909806e86a538decb75d
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-02-27 21:26:32 -0300

    atividade de calculo de perimetro

--- index.cpp
+++ index.cpp
@@ -4,28 +4,16 @@ using namespace std;
 int main ()
 {
 
-	int a;
-	int b;
-
-	cout << "Digite um numero de 1 a 10 \n";
-	cin >> a;
-	cout << "Digite outro numero de 1 a 10 \n";
-	cin >> b;
-	if (a%2 == 0)
-	{
-		cout << "O primeiro numero e par \n";
-	}
-	else
-	{
-		cout << "O primeiro numero e impar \n";
-	}
-		if (b%2 == 0)
-	{
-		cout << "O segundo numero e par \n";
-	}
-	else
-	{
-		cout << "O segundo numero e impar \n";
-	}
+	float PI = 3.14;
+	float raio;
+	float perimetro;
+	float area;
+	
+	cout << "Digite abaixo o numero do raio \n";
+	cin >> raio;
+	perimetro = 2 * PI * raio;
+	area = PI * (raio*raio);
+	cout << "O perimetro corresponde a... " << perimetro << "\n";
+	cout << "A area corresponde a... " << area;
 	
 }
