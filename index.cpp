4c825e3471ba7a716037628235af6784b4ac5f19
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-02-27 21:50:17 -0300

    usando a biblioteca cmath com potenciação

--- index.cpp
+++ index.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 #define PI 3.14
 
 int main ()
 {
 
-	float raio;
-	float perimetro;
-	float area;
+	int raio;
+	float perimetro, area;
 	
 	cout << "Digite abaixo o numero do raio \n";
 	cin >> raio;
+	
 	perimetro = 2 * PI * raio;
-	area = PI * (raio*raio);
+	area = PI * pow(raio, 2);
+	
 	cout << "O perimetro corresponde a... " << perimetro << "\n";
 	cout << "A area corresponde a... " << area;
 	
